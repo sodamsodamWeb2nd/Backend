ced08337837c8323a5dd0a2419388bfd253abcc3
tuioe <qodlgks123@naver.com>
2025-05-24 15:38:14 +0900

    feat: 리뷰 전체 조회 API 기본 구조 및 응답 DTO, Swagger 설정
    - ReviewController 클래스 정의 및 GET /reviews 엔드포인트 추가
    - ReviewSummaryResponse DTO 정의
    - Swagger 문서화 어노테이션 적용 (@Tag, @Operation, @Schema 등

--- src/main/java/com/example/sodamsodam/apps/review/controller/ReviewController.java
+++ src/main/java/com/example/sodamsodam/apps/review/controller/ReviewController.java
@@ -2,6 +2,7 @@ package com.example.sodamsodam.apps.review.controller;
 
 import com.example.sodamsodam.apps.review.dto.ReviewCreateRequest;
 import com.example.sodamsodam.apps.review.dto.ReviewCreateResponse;
+import com.example.sodamsodam.apps.review.dto.ReviewSummaryResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -12,6 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -32,5 +35,18 @@ public class ReviewController {
         // 리뷰 ID 값은 나중에 기능 구현후 생성된 리뷰 ID 값으로 대체 예정
         ReviewCreateResponse response = new ReviewCreateResponse(1L,"리뷰가 성공적으로 등록되었습니다");
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @Operation(summary = "리뷰 전체 조회", description = "해당 장소에 리뷰를 전체 조회합니다")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "리뷰 조회 성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 장소"),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+    @GetMapping("/{place_id}/reviews")
+    public ResponseEntity<List<ReviewSummaryResponse>> getReviews(@PathVariable Long place_id) {
+        // 현재는 아무런 값이 없는 응답 DTO만 반환 -> 추후 로직 구현 단계에서 추가 예정
+        ReviewSummaryResponse response = new ReviewSummaryResponse();
+        return ResponseEntity.status(HttpStatus.OK).body(Collections.singletonList(response));
     }
 }
